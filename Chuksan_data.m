bbce58988c4073f9d0bca57bbf7ac6bbdaa8084d
Thakurkorea <52812653+Thakurkorea@users.noreply.github.com>
2022-01-06 10:42:50 +0900

    Add files via upload

---
+++ Chuksan_data.m
@@ -0,0 +1,59 @@
+clc
+clear all
+import java.nio.charset.Charset
+encoding = Charset.defaultCharset()
+load chuksan.mat
+load city_coordinates.mat
+imagesc(chuksan)
+colorbar
+%lables={'??','??','??','??','??','??','??','??','??','???','???','???','???','???','???','??','??'}
+lables={'??','??','??',	'??',	'??',	'??',	'??',	'??',	'??',	'???'	,'???',	'???'	,'???',...
+    '???',	'???',	'??',	'??'}
+xticklabels(lables)
+yticklabels(lables)
+xticks(1:1:length(chuksan))
+yticks(1:1:length(chuksan))
+nodenames=lables
+% let's see the inter city graph for livestock transport 
+G=digraph(chuksan,nodenames)
+p=plot(G)  
+p.EdgeCData=G.Edges.Weight; colormap jet; colorbar
+p.EdgeFontWeight= 'bold'
+p.NodeFontWeight= 'bold'
+p.EdgeColor='flat'
+p.NodeFontSize=10
+
+% let's find the inter city distances  in LM
+dist_data=zeros(length(City_cord));
+for i=1:length(City_cord);
+    for j=1:length(City_cord);
+        distn=acos(cos(deg2rad(90-City_cord(i,2))) * cos(deg2rad(90-City_cord(j,2)))...
+            + sin(deg2rad(90-City_cord(i,2))) * sin(deg2rad(90-City_cord(j,2)))...
+            * cos(deg2rad(City_cord(i,1)-City_cord(j,1)))) * 6371
+        dist_data(i,j)=real(distn)
+
+    end
+end 
+
+% plot the distance network figure
+figure
+G=digraph(dist_data,nodenames)
+p=plot(G)  
+p.EdgeCData=G.Edges.Weight; colormap jet; colorbar
+p.EdgeFontWeight= 'bold'
+p.NodeFontWeight= 'bold'
+p.EdgeColor='flat'
+p.NodeFontSize=10
+% plot the heatmap for distances in KM
+figure 
+imagesc(dist_data)
+colorbar
+xticklabels(lables)
+yticklabels(lables)
+xticks(1:1:length(chuksan))
+yticks(1:1:length(chuksan))
+
+
+
+
+
